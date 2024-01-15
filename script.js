686a83ea32f40782616375299729eb3e0eea11df
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-01-15 10:24:44 -0300

    revisando o script

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuBtnOpen = document.getElementById("menuBtn");
-    const menuBtnClose = document.getElementById("closeBtn");
-    const menu = document.getElementById("menu");
 
+    document.addEventListener("scroll", showArrow);
     menuBtnOpen.addEventListener("click", openMenu);
-    menuBtnClose.addEventListener("click", closeMenu);
-    menu.addEventListener("click", closeMenu);
 });
+
+function showArrow() {
+    const arrow = document.getElementById("arrow");
+    let currentScroll = window.scrollY;
+    if (currentScroll > 0) {
+        arrow.classList.remove("hidden");
+        arrow.classList.add("block");
+    } else if (currentScroll === 0) {
+        arrow.classList.add("hidden");
+        arrow.classList.remove("block");
+    }
+}
 
 function openMenu() {
     const blurContent = document.getElementById("blur");
@@ -16,14 +25,14 @@ function openMenu() {
 
     menuBtnOpen.removeEventListener("click", openMenu);
 
-    blurContent.className = "blur"
-
+    blurContent.className = "blur";
     menu.style.animation = "slideOpen linear 0.25s";
-
     menu.classList.remove("hidden");
     menu.classList.add("flex");
 
     menuBtnClose.addEventListener("click", closeMenu);
+
+    document.addEventListener("scroll", closeMenu);
 }
 
 function closeMenu() {
@@ -33,8 +42,7 @@ function closeMenu() {
     const menuBtnOpen = document.getElementById("menuBtn");
 
     menuBtnClose.removeEventListener("click", closeMenu);
-
-    blurContent.className = ""
+    blurContent.className = "";
 
     menu.style.animation = "slideClose linear 0.25s";
 
